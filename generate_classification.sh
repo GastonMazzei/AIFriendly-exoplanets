84f70015d277821f41edaed4c1b73b18433887ab
Gaston Mazzei <gastonmazzei95@gmail.com>
2020-10-30 11:03:34 -0300

    Final Version

--- generate_classification.sh
+++ generate_classification.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir gallery
 echo "AI-Friendly is about to learn which planets have orbital periods longer than a human life (80 years)"
 python3 scripts/aifriendly.py 32 50 >> logs.txt
 echo "Done! results saved in 'gallery/results.png'"
